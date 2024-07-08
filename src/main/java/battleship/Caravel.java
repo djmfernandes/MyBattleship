68914a811159826e9885407610b65c177a3584c5
José Reis <jvpreis@gmail.com>
2024-07-08 19:35:34 +0100

    Update Caravel.java
    delete Caravel description

--- src/main/java/battleship/Caravel.java
+++ src/main/java/battleship/Caravel.java
@@ -9,7 +9,7 @@ public class Caravel extends Ship
     private static final String NAME = "Caravela";
 
     /**
-     * @param bearing the bearing where the Caravel heads to don't remember
+     * @param bearing the bearing where the Caravel heads to 
      * @param pos     initial point for positioning the Caravel
      */
     public Caravel(Compass bearing, IPosition pos) throws NullPointerException, IllegalArgumentException
